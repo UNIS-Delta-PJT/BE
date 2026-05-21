8c137a03422c02680b96019ce218467eaf0b6c81
Lee Hyunkyung <michelle3hg@gmail.com>
2026-05-21 20:31:02 +0900

    test: 임시 사용자 생성 테스트까지 완

--- delta/src/main/java/unis/project/delta/character/repository/CharacterRepository.java
+++ delta/src/main/java/unis/project/delta/character/repository/CharacterRepository.java
@@ -1,6 +1,7 @@
 package unis.project.delta.character.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import unis.project.delta.character.domain.Character;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
 }
